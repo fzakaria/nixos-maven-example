adce0a4ca3bce52048a43c1c4b550abd8551a4ec
Farid Zakaria <fmzakari@google.com>
2020-10-19 13:15:29 -0700

    Fix runnable-jar

--- runnable-jar.nix
+++ runnable-jar.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { }, stdenv ? pkgs.stdenv, lib ? pkgs.lib
 , maven ? pkgs.maven, callPackage ? pkgs.callPackage
-, makeWrapper ? pkgs.makeWrapper }:
+, makeWrapper ? pkgs.makeWrapper, jre ? pkgs.jre }:
 # pick a repository derivation, here we will use buildMaven
 let repository = callPackage ./build-maven-repository.nix { };
 in stdenv.mkDerivation rec {
@@ -17,11 +17,11 @@ in stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
+    mkdir -p $out/bin
     # create a symbolic link for the repository directory
     ln -s ${repository} $out/repository
 
     install -Dm644 target/${pname}-${version}.jar $out/share/java
-    mkdir -p $out/bin
     # create a wrapper that will automatically set the classpath
     # this should be the paths from the dependency derivation
     makeWrapper ${jre}/bin/java $out/bin/${pname} \
